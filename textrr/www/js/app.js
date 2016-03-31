0635f0d8cc3d57b67ef964f5de93733f7c7fd793
schriste92 <sjchristensen1@gmail.com>
2016-03-31 10:54:47 -0500

    local storage working

--- textrr/www/js/app.js
+++ textrr/www/js/app.js
@@ -1,6 +1,6 @@
-angular.module('textrr', ['ionic'])
+var app = angular.module('textrr', ['ionic'])
 
-.run(function($ionicPlatform) {
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
@@ -12,8 +12,20 @@ angular.module('textrr', ['ionic'])
   });
 })
 
-var words = ["want","ipad","help","love","colors","yes","no","bath","school","read","hug","brush teeth","ball","music","potty","up",
-"snack","sing","alphabet","walk","milk","mama","daddy","go","car","numbers","more","blanket","stop"];
+var default_words = [
+  "want","ipad","help","love","colors","yes","no","bath","school","read","hug","brush teeth",
+  "ball","music","potty","up","snack","sing","alphabet","walk","milk","mama","daddy","go","car","numbers","more","blanket","stop"
+];
+
+var words;
+
+document.addEventListener("deviceready", function(){
+  if(window.localStorage['words'] === undefined){
+    window.localStorage['words'] = default_words.toString();
+  }
+  words = window.localStorage['words'].split(",");
+});
+
 
 document.getElementById("playbutton").addEventListener('click', function () {
     TTS
